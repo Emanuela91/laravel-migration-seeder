079383cbb45ddafa66b1b4b2753af421ae3c9f63
Emanuela91 <emanuela.marson91@gmail.com>
2023-02-01 17:16:52 +0100

    creata la tabella con le sue colonne e caratteristiche

--- database/migrations/2023_02_01_160115_travel_table.php
+++ database/migrations/2023_02_01_160115_travel_table.php
@@ -13,8 +13,15 @@ return new class extends Migration {
     public function up()
     {
         // qui creo la tabella con le sue colonne
-        Schema::create('train', function (Blueprint $table) {
-
+        Schema::create('travel', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->integer('travel_number')->unique();
+            $table->string('city', 64);
+            $table->date('check_in')->useCurrent();
+            $table->date('check_out')->useCurrent();
+            $table->integer('person')->unsigned();
+            $table->string('hotel', 32);
+            $table->integer('price')->unsigned();
             $table->timestamps();
         });
     }
