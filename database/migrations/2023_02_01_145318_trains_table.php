8cbd03754b17ca45f85f7ef971ebee0b845a01b6
Emanuela91 <emanuela.marson91@gmail.com>
2023-02-01 16:57:33 +0100

    creata la tabella train su php myadmin

--- database/migrations/2023_02_01_145318_trains_table.php
+++ database/migrations/2023_02_01_145318_trains_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use PhpParser\Node\NullableType;
 
 return new class extends Migration {
     /**
@@ -12,13 +13,14 @@ return new class extends Migration {
      */
     public function up()
     {
+        // qui creo la tabella con le sue colonne
         Schema::create('train', function (Blueprint $table) {
-            $table->id();
-            $table->string('name', 32);
-            $table->integer('train_number');
-            $table->date('departures');
-            $table->date('arrivals');
-            $table->integer('price');
+            $table->id()->autoIncrement();
+            $table->string('name', 32)->nullable();
+            $table->integer('train_number')->unique();
+            $table->date('departures')->useCurrent();
+            $table->date('arrivals')->useCurrent();
+            $table->integer('price')->unsigned();
             $table->timestamps();
         });
     }
@@ -30,6 +32,7 @@ return new class extends Migration {
      */
     public function down()
     {
+        // qui elimino le colonne che ho creato
         Schema::dropIfExists('train');
     }
 };
