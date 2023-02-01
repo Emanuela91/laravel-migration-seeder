8927b321af97f30678c0f114c243d4ec0c749bbb
Emanuela91 <emanuela.marson91@gmail.com>
2023-02-01 16:09:47 +0100

    aggiunte caratteristiche della tabella

---
+++ database/migrations/2023_02_01_145318_trains_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('train', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 32);
+            $table->integer('train_number');
+            $table->date('departures');
+            $table->date('arrivals');
+            $table->integer('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('train');
+    }
+};
